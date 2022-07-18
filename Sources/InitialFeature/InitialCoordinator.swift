9992a0ddcd2b88467837e0ffe2c2b163fb3712ca
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-07-17 20:51:49 -0500

    Clean file

--- Sources/InitialFeature/InitialCoordinator.swift
+++ Sources/InitialFeature/InitialCoordinator.swift
@@ -9,65 +9,53 @@
 import UIKit
 import Shared
 
-// MARK: - TODO, separate files
-
-public protocol InitialCoordinatorProtocol: class {
-  
+public protocol InitialCoordinatorProtocol: AnyObject {
   func navigate(to step: InitialStep)
 }
 
-public protocol InitialCoordinatorDelegate: class {
+public protocol InitialCoordinatorDelegate: AnyObject {
   func initialCoordinatorDidFinish()
 }
 
 public enum InitialStep: Step {
-  case
-  
-  initialSceneIsRequired,
-  
-  initialSceneDidFinish
+  case initialSceneIsRequired
+  case initialSceneDidFinish
 }
 
 public protocol InitialCoordinatorDependencies {
-  
   func buildInitialViewController(coordinator: InitialCoordinatorProtocol?) -> UIViewController
 }
 
 public class InitialCoordinator: Coordinator, InitialCoordinatorProtocol {
-  
   weak var delegate: InitialCoordinatorDelegate?
-  
   private let tabBarController: UITabBarController
-  
   private let dependencies: InitialCoordinatorDependencies
-  
+
   // MARK: - Life Cycle
-  
   public init(tabBarController: UITabBarController, dependencies: InitialCoordinatorDependencies) {
     self.tabBarController = tabBarController
     self.dependencies = dependencies
   }
-  
+
   deinit {
     print("deinit \(Self.self)")
   }
-  
+
   public func start(with step: InitialStep) {
     navigate(to: step)
   }
-  
+
   // MARK: - Navigation
-  
   public func navigate(to step: InitialStep) {
     switch step {
     case .initialSceneIsRequired:
       showInitialFeature()
-      
+
     case .initialSceneDidFinish:
       delegate?.initialCoordinatorDidFinish()
     }
   }
-  
+
   fileprivate func showInitialFeature() {
     let initialVC = dependencies.buildInitialViewController(coordinator: self)
     tabBarController.setViewControllers([initialVC], animated: true)
