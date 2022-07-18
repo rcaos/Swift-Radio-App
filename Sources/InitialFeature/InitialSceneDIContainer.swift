b9ae11d0fac1328e01f188cbf47e8b09b6486c75
Jeans Ruiz <jeansruiz.c@gmail.com>
2022-07-17 22:34:19 -0500

    expose module dependencies

--- Sources/InitialFeature/InitialSceneDIContainer.swift
+++ Sources/InitialFeature/InitialSceneDIContainer.swift
@@ -10,18 +10,21 @@ import Domain
 import NetworkingInterface
 import UIKit
 
-final class InitialSceneDIContainer {
-  
-  struct Dependencies {
+public final class InitialSceneDIContainer {
+
+  public struct Dependencies {
     let dataTransferService: DataTransferService
 //    let stationsLocalStorage: StationsLocalStorage
+
+    public init(dataTransferService: DataTransferService) {
+      self.dataTransferService = dataTransferService
+    }
   }
 
   private let dependencies: Dependencies
-  
+
   // MARK: - Initializers
-  
-  init(dependencies: Dependencies) {
+  public init(dependencies: Dependencies) {
     self.dependencies = dependencies
   }
   
@@ -31,26 +34,25 @@ final class InitialSceneDIContainer {
 }
 
 // MARK: - Private
-
 extension InitialSceneDIContainer {
-  
+
   private func makeInitialViewModel(coordinator: InitialCoordinatorProtocol?) -> InitialViewModel {
-//    let initialVM = InitialViewModel(fetchStationsUseCase: makeFetchStationsUseCase())
-    let initialVM = InitialViewModel()
+    let initialVM = InitialViewModel(fetchStationsUseCase: makeFetchStationsUseCase())
     initialVM.coordinator = coordinator
     return initialVM
   }
   
   // MARK: - Use Cases
   
-//  private func makeFetchStationsUseCase() -> FetchStationsUseCase {
-//    return DefaultFetchStationsUseCase(
-//      stationsRepository: makeStationsRepository(),
-//      stationsLocalRepository: makeStationsLocalRepository())
-//  }
+  private func makeFetchStationsUseCase() -> FetchStationsUseCase {
+    return DefaultFetchStationsUseCase(
+      stationsRemoteRepository: StationsRepository(dataTransferService: dependencies.dataTransferService)
+//      ,
+//      stationsLocalRepository: makeStationsLocalRepository()
+    )
+  }
 //
 //  // MARK: - Repositories
-//
 //  private func makeStationsRepository() -> StationsRepository {
 //    return DefaultStationsRepository(dataTransferService: dependencies.dataTransferService)
 //  }
