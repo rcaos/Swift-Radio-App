cc5686001850d60075c8a1bf0d73afcbada85cc6
Jeans <jeansruiz.c@gmail.com>
2023-08-14 09:41:07 -0500

    setup packages

--- App/Package.swift
+++ App/Package.swift
@@ -5,16 +5,41 @@ import PackageDescription
 
 let package = Package(
   name: "RadioApp",
+  platforms: [
+    .iOS(.v16)
+  ],
   products: [
-    .library(name: "AppFeature", targets: ["AppFeature"])
+    .library(name: "AppFeature", targets: ["AppFeature"]),
+    .library(name: "Networking", targets: ["Networking"]),
+    .library(name: "NetworkingLive", targets: ["NetworkingLive"])
   ],
   targets: [
     .target(
-      name: "AppFeature"
+      name: "AppFeature",
+      dependencies: [
+        "NetworkingLive"
+      ]
+    ),
+    .target(
+      name: "Networking"
+    ),
+    .target(
+      name: "NetworkingLive",
+      dependencies: [
+        "Networking"
+      ]
     ),
     .testTarget(
       name: "AppFeatureTests",
-      dependencies: ["AppFeature"]
+      dependencies: [
+        "AppFeature"
+      ]
+    ),
+    .testTarget(
+      name: "NetworkingTests",
+      dependencies: [
+        "NetworkingLive"
+      ]
     )
   ]
 )
