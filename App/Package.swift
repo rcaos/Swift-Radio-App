47d4405fe2f5accae385596fec438537d6ddbad1
Jeans <jeansruiz.c@gmail.com>
2023-08-14 18:27:26 -0500

    order package

--- App/Package.swift
+++ App/Package.swift
@@ -10,15 +10,25 @@ let package = Package(
   ],
   products: [
     .library(name: "AppFeature", targets: ["AppFeature"]),
+    .library(name: "AudioPlayerClient", targets: ["AudioPlayerClient"]),
     .library(name: "Networking", targets: ["Networking"]),
     .library(name: "NetworkingLive", targets: ["NetworkingLive"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-async-algorithms.git", .upToNextMajor(from: .init(stringLiteral: "0.1.0")) )
   ],
   targets: [
     .target(
       name: "AppFeature",
       dependencies: [
-        "NetworkingLive"
+        "NetworkingLive",
+        "AudioPlayerClient",
+        .product(name: "AsyncAlgorithms", package: "swift-async-algorithms")
       ]
+    ),
+    .target(
+      name: "AudioPlayerClient",
+      dependencies: []
     ),
     .target(
       name: "Networking"
