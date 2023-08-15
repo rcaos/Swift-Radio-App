6fbb136bcdd626c41fceb6b828ddaecce7c1edb1
Jeans <jeansruiz.c@gmail.com>
2023-08-15 12:10:18 -0500

    Increase iOS version

--- App/Package.swift
+++ App/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "RadioApp",
   platforms: [
-    .iOS(.v16)
+    .iOS(.v17)
   ],
   products: [
     .library(name: "AppFeature", targets: ["AppFeature"]),
