c5bffed93b36c5d2a5cfa2e84d810d02ee3bebb1
Jeans <jeansruiz.c@gmail.com>
2023-10-02 10:57:22 -0500

    order memory source

--- App/Sources/PlayerFeature/Data/DataSources/RadioStationsMemoryDataSource.swift
+++ App/Sources/PlayerFeature/Data/DataSources/RadioStationsMemoryDataSource.swift
@@ -9,20 +9,20 @@ extension RadioStationsRemoteDataSource {
 
     let memoryStations: [RadioStationRemote] = [
       .init(
-        id: "94421162-729F-47B7-983D-B7F40497662A",
-        name: "RPP",
-        description: "Confianza por todos los medios",
-        audioStreamURL: URL(string: "http://cassini.shoutca.st:9300/stream")!,
-        pathImageURL: URL(string: "https://www.dropbox.com/s/f8du4p4c7n90axt/rpp.png?dl=1")!,
-        type: .other
-      ),
-      .init(
         id: "47",
         name: "Studio 92",
         description: "Número 1 en tu música",
         audioStreamURL: URL(string: "https://mdstrm.com/audio/5fada553978fe1080e3ac5ea/icecast.audio")!,
         pathImageURL: URL(string: "https://www.dropbox.com/s/hgxrkhe8ar40360/studio92.jpg?dl=1")!,
         type: .RPP
+      ),
+      .init(
+        id: "94421162-729F-47B7-983D-B7F40497662A",
+        name: "RPP",
+        description: "Confianza por todos los medios",
+        audioStreamURL: URL(string: "http://cassini.shoutca.st:9300/stream")!,
+        pathImageURL: URL(string: "https://www.dropbox.com/s/f8du4p4c7n90axt/rpp.png?dl=1")!,
+        type: .other
       ),
       .init(
         id: "06EDB6E9-EE8B-426A-A915-12FAC490766B",
