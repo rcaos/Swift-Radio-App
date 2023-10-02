4be81cb63e1860674459923645f01ee99fdd789f
Jeans <jeansruiz.c@gmail.com>
2023-10-02 09:29:05 -0500

    add helper to test

--- App/Sources/PlayerFeature/PlayerViewModel+Test.swift
+++ App/Sources/PlayerFeature/PlayerViewModel+Test.swift
@@ -1,8 +1,27 @@
 //
-//  File.swift
-//  
-//
 //  Created by Jeans Ruiz on 2/10/23.
 //
 
 import Foundation
+import XCTestDynamicOverlay
+
+#if DEBUG
+extension PlayerViewModel {
+
+  public static func test(
+    fetchNowInfoUseCase: @escaping () -> FetchNowInfoUseCase = { unimplemented("fetchNowInfoUseCase") },
+    fetchAllRadioStations: @escaping () -> FetchAllRadioStations = { unimplemented("fetchAllRadioStations") },
+    getRadioStationById: @escaping () -> GetRadioStationById = { unimplemented("getRadioStationById") },
+    toggleFavoriteRadioStationUseCase: @escaping () -> ToggleRadioStationFavoriteUseCase = { unimplemented("toggleFavoriteRadioStationUseCase") },
+    fetchAllFavorites: @escaping () -> FetchAllFavoriteRadiosUseCase = { unimplemented("fetchAllFavorites") }
+  ) -> PlayerViewModel {
+    return PlayerViewModel(
+      fetchNowInfoUseCase: fetchNowInfoUseCase,
+      fetchAllRadioStations: fetchAllRadioStations,
+      getRadioStationById: getRadioStationById,
+      toggleFavoriteRadioStationUseCase: toggleFavoriteRadioStationUseCase,
+      fetchAllFavorites: fetchAllFavorites
+    )
+  }
+}
+#endif
